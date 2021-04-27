b8e33dd54d0450e7b7e9b857f50c23bb9b01cb3b
vladciuca <vladciuca@gmail.com>
2021-04-27 13:48:30 +0300

    added icon on currency slect -global bar

--- src/components/CurrencySelect/CurrencySelect.styles.js
+++ src/components/CurrencySelect/CurrencySelect.styles.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
+export const Label = styled.label`
+  display: flex;
+  align-items: center;
+`;
+
 export const Select = styled.select`
+  margin-left: 0.2rem;
   background: #191320;
   color: #5b486a;
   border: none;
