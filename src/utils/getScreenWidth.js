1330fbaccc9080db0aa4183a82374738278904e6
Vlad Cristian <vladccdev@gmail.com>
2021-08-01 18:24:40 +0300

    fixed skeleton chart for xs devices

--- src/utils/getScreenWidth.js
+++ src/utils/getScreenWidth.js
@@ -1,6 +1,6 @@
 const getScreenWidth = () => {
   const width = window.innerWidth;
-  if (width < 646) return 5;
+  if (width < 646) return 4;
   if (width < 992 && width > 576) return 8;
   return 12;
 };
